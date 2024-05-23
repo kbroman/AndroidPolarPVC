a146f8bbbf87c572b143ef0246b87f07c49d13fb
Karl Broman <kbroman@gmail.com>
2024-05-22 19:32:54 -0500

    Further simplify RunningAveSD.add()

--- app/src/main/java/org/kbroman/polar/polarpvc/RunningAveSD.java
+++ app/src/main/java/org/kbroman/polar/polarpvc/RunningAveSD.java
@@ -14,12 +14,9 @@ public class RunningAveSD extends RunningAverage {
     @Override public void add(double val) {
         if (list.size() == maxItems) {
             double first = list.getFirst();
-            sum -= first;
             sumsq -= (first*first);
-            list.removeFirst();
         }
-        list.add(val);
-        sum += val;
+        super.add(val);
         sumsq += (val*val);
     }
 
